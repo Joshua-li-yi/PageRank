732cb3f617939735ececffb49faf1b9756ed051f
Joshua_yi <55046924+Joshua-li-yi@users.noreply.github.com>
2020-04-08 21:43:59 +0800

    Update pageRank2.py
    add notes

--- pageRank2.py
+++ pageRank2.py
@@ -13,7 +13,8 @@ pd.set_option('display.max_columns', 1000)
 pd.set_option('display.width', 1000)
 pd.set_option('display.max_colwidth', 1000)
 
-
+# 从txt导入数据、将数据转化成 nodes 输入和输出，类似于将边存起来
+# 需要进行优化，速度有点慢
 def load_data(filePath):
     nodes = pd.DataFrame(columns=['input_node', 'output_node'])
     all_node = []
@@ -49,18 +50,20 @@ def load_data(filePath):
     all_node.sort()
     return nodes ,all_node
 
+# 生成rank值
 def generate_rank(all_node):
     # 初始rank
     initial_rank_old = 1/len(all_node)
     rank = pd.DataFrame({'page':all_node,'score':initial_rank_old},columns=['page','score'])
-    # 将page列设置为索引
-
+    
+    # 这个有点问题，得查查怎么改
     tqdm.pandas(desc="rank initial")
     rank.progress_apply(lambda x: x ** 2)
+    # 将page列设置为索引
     rank.set_index('page',inplace=True)
     return rank
 
-
+# 将之前得nodes 存起来得边，转化为矩阵。用的是老师PPT上的'source_node','degree','destination_nodes'结构
 def nodes_to_M(nodes):
     M = pd.DataFrame(columns=['source_node','degree','destination_nodes'])
     # 将M的source_node列设置为索引
@@ -77,7 +80,7 @@ def nodes_to_M(nodes):
             bar.update(1)
     return M
 
-
+# 将一个列表划分为多个小列表
 def list_to_groups(list_info, per_list_len):
     '''
     :param list_info:   列表
@@ -95,11 +98,13 @@ def list_to_groups(list_info, per_list_len):
 
 
 def block_strip(M,block_node_groups):
+    # 存最后的各个划分后的M
     M_block_stripe = []
     with tqdm(total=len(block_node_groups),desc='block strip progress') as bar:
         for node_group in block_node_groups:
             temp_block_M = pd.DataFrame(columns=['source_node', 'degree', 'destination_nodes'])
             temp_block_M.set_index('source_node', inplace=True)
+            # 将大的M 根据 划分后的node节点，进行块条化最后结果存到M_block_stripe列表中
             for per_node in node_group:
                 for index,row in M.iterrows():
                     if per_node in row['destination_nodes'].tolist():
@@ -115,12 +120,12 @@ def block_strip(M,block_node_groups):
     return M_block_stripe
 # print(M_block_stripe)
 
-
+# 计算每个节点的入度，暂时没有用上
 def comput_node_input_time(nodes):
     node_input_time = nodes.apply(pd.value_counts)['output_node']
     return node_input_time
 
-
+# 计算pagerank值
 def pageRank(block_stripe_M, old_rank,all_node):
     initial_rank_new = (1-Beta)/ len(all_node)
     new_rank = pd.DataFrame({'page': all_node, 'score': initial_rank_new}, columns=['page', 'score'])
@@ -135,7 +140,7 @@ def pageRank(block_stripe_M, old_rank,all_node):
                 for per_node in node_list:
                     new_rank.loc[per_node,'score'] += Beta*old_rank.loc[index,'score']/row['degree']
     return new_rank
-
+# 相当于main，输入文件路径，输出rank值
 def mypageRank(file):
     file_path = file
     nodes, all_node = load_data(file_path)
@@ -159,7 +164,7 @@ def mypageRank(file):
     new_rank = pageRank(M_block_stripe, rank,all_node)
     return new_rank
 
-
+# 线程相关
 class myThread (threading.Thread):
     def __init__(self, threadID, name):
         threading.Thread.__init__(self)
